377d9293848292aa1d03e395132f50c38122b46d
Meraj Latifli <meracletifli@outlook.com>
2021-03-09 17:25:28 +0400

    Add project files.

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_LogInCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { 
+       
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Username";
+                textBox1.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Username")
+            {
+                textBox1.Text = null;
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Password")
+            {
+                textBox2.Text = null;
+                textBox2.ForeColor = Color.Black;
+                this.textBox2.PasswordChar = '*';
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "Password";
+                textBox2.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text == "admin")
+            {
+                label2.Text = "Correct";
+            }
+
+            if (textBox1.Text != "admin" || textBox2.Text != "admin")
+            {
+                label2.Text =  "InCorrect";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
